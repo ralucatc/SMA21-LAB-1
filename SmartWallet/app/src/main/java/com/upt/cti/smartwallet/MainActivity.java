52b58556465cb688dba366a62b4b109288ab4093
Raluca <carare.raluca27@gmail.com>
2021-11-21 14:52:26 +0200

    lab8

--- SmartWallet/app/src/main/java/com/upt/cti/smartwallet/MainActivity.java
+++ SmartWallet/app/src/main/java/com/upt/cti/smartwallet/MainActivity.java
@@ -1,18 +1,17 @@
 package com.upt.cti.smartwallet;
-import static java.lang.Float.parseFloat;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Context;
-import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.Spinner;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -20,143 +19,77 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.ResourceBundle;
 
-import model.MonthlyExpenses;
+import model.Payment;
+import ui.PaymentAdapter;
 
-public class MainActivity extends AppCompatActivity implements View.OnCreateContextMenuListener, AdapterView.OnItemSelectedListener{
+public class MainActivity extends AppCompatActivity  {
+
+    private int currentMonth;
+    private List<Payment> payments = new ArrayList<>();
+    private DatabaseReference databaseReference;
 
     private TextView tStatus;
-    private EditText eIncome, eExpenses;
-    private Spinner mSpinner;
-    private DatabaseReference databaseReference;
-    private String currentMonth;
-    private ValueEventListener databaseListener;
-    private List<String> monthsArray;
-    private final static String PREFERENCES_SETTINGS = "prefs_settings";
-    private SharedPreferences sharedPreferences;
+    private Button bPrevious;
+    private Button bNext;
+    private FloatingActionButton fabAdd;
+    private ListView listPayments;
+
+    private ValueEventListener databaseListener = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tStatus = (TextView) findViewById(R.id.textView);
-        eIncome = (EditText) findViewById(R.id.eIncome);
-        eExpenses = (EditText) findViewById(R.id.eExpenses);
+        tStatus = (TextView) findViewById(R.id.tStatus);
+        bPrevious = (Button) findViewById(R.id.bPrevious);
+        bNext = (Button) findViewById(R.id.bNext);
+        fabAdd = (FloatingActionButton) findViewById(R.id.fabAdd);
+        listPayments = (ListView) findViewById(R.id.listPayments);
+        final PaymentAdapter adapter = new PaymentAdapter(this, R.layout.item_payment, payments);
+        listPayments.setAdapter(adapter);
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance("https://smart-wallet-27310-default-rtdb.europe-west1.firebasedatabase.app/");
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://smart-wallet-d5bb5-default-rtdb.europe-west1.firebasedatabase.app");
         databaseReference = database.getReference();
-
-        monthsArray = new ArrayList<String>();
-        mSpinner = (Spinner) findViewById(R.id.Mspinner);
-        sharedPreferences =  getSharedPreferences(PREFERENCES_SETTINGS, Context.MODE_PRIVATE);
-
-        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, monthsArray);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mSpinner.setAdapter(adapter);
-        mSpinner.setOnItemSelectedListener(this);
-
-        databaseReference.child("Calendar").addValueEventListener(new ValueEventListener() {
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                System.out.println("Add");
-                for(DataSnapshot ds : dataSnapshot.getChildren()) {
-
-                    MonthlyExpenses monthlyExpenses = ds.getValue(MonthlyExpenses.class);
-                    monthlyExpenses.month = ds.getKey();
-                    System.out.println("Value" + monthlyExpenses.getMonth());
-                    if(monthlyExpenses.getMonth() != null) monthsArray.add(ds.getKey());
-                }
-                if(currentMonth != null) mSpinner.setSelection(monthsArray.indexOf(currentMonth));
-
-                adapter.notifyDataSetChanged();
-            }
-
+        databaseReference.child("wallet").addChildEventListener(new ChildEventListener() {
             @Override
-            public void onCancelled(DatabaseError error) {
-            }
-        });
-
-        currentMonth=sharedPreferences.getString("CurrentMonth", null);
-    }
-
-    public void clicked(View view) {
-        switch (view.getId()) {
-            case R.id.bUpdate:
-                if(!eIncome.getText().toString().isEmpty() && !eExpenses.getText().toString().isEmpty()){
-                    tStatus.setText("Searching ...");
-                    createnewUpdateDbListener();
-                }
-                break;
-        }
-    }
-
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        Object selectedMonth = parent.getItemAtPosition(position);
-        currentMonth=selectedMonth.toString();
-        sharedPreferences.edit().putString("CurrentMonth", currentMonth).apply();
-        createNewDBListener();
-    }
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-        createNewDBListener();
-    }
-
-    private void createNewDBListener() {
-        // remove previous databaseListener
-        if (databaseReference != null && currentMonth != null && databaseListener != null)
-            databaseReference.child("calendar").child(currentMonth).removeEventListener(databaseListener);
-
-        databaseListener = new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-
-                MonthlyExpenses monthlyExpense = dataSnapshot.getValue(MonthlyExpenses.class);
-                // explicit mapping of month name from entry key
-                monthlyExpense.month = dataSnapshot.getKey();
-
-                eIncome.setText(String.valueOf(monthlyExpense.getIncome()));
-                eExpenses.setText(String.valueOf(monthlyExpense.getExpenses()));
-                tStatus.setText("Found entry for " + currentMonth);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-            }
-        };
-        databaseReference.child("calendar").child(currentMonth).addValueEventListener(databaseListener);
-    }
-
-    private void createnewUpdateDbListener() {
-        // remove previous databaseListener
-        if (databaseReference != null && currentMonth != null && databaseListener != null)
-            databaseReference.child("calendar").child(currentMonth).removeEventListener(databaseListener);
-
-        databaseListener = new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                MonthlyExpenses monthlyExpense = new MonthlyExpenses(currentMonth, parseFloat(eIncome.getText().toString()),  parseFloat(eExpenses.getText().toString()));
-                // whenever data at this location is updated.
-                for(DataSnapshot ds : dataSnapshot.getChildren()) {
-                    if (currentMonth == dataSnapshot.getKey()) {
-                        dataSnapshot.child("income").getRef().setValue(monthlyExpense.getIncome());
-                        dataSnapshot.child("expenses").getRef().setValue(monthlyExpense.getExpenses());
-                        // explicit mapping of month name from entry key
+            public void onChildAdded(DataSnapshot snapshot,String previousChildName) {
+                Payment newPayment = snapshot.getValue(Payment.class);
+                if (newPayment != null) {
+                    newPayment.timestamp = snapshot.getKey();
+                    if (!payments.contains(newPayment))
+                    {
+                        payments.add(newPayment);
                     }
+                    adapter.notifyDataSetChanged();
                 }
-                tStatus.setText("Found entry for " + currentMonth);
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot snapshot,String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot snapshot,String previousChildName) {
+
             }
 
             @Override
             public void onCancelled(DatabaseError error) {
+
             }
-        };
+        }) ;
 
-        // set new databaseListener
-        databaseReference.child("calendar").child(currentMonth).addValueEventListener(databaseListener);
+
     }
-
 }
---
+++ SmartWallet/app/src/main/java/model/Payment.java
@@ -0,0 +1,27 @@
+package model;
+import com.google.firebase.database.IgnoreExtraProperties;
+@IgnoreExtraProperties
+
+public class Payment {
+
+    public String timestamp;
+    private double cost;
+    private String name;
+    private String type;
+
+    public Payment() {
+        // Default constructor required for calls to DataSnapshot.getValue(User.class)
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
